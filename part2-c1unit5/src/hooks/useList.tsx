d96de178c8fdd9fc3c93dcf7cac87d5f006d5e25
kothadiyasagar <sagarkothadiya98@gmail.com>
2022-07-11 16:04:55 +0530

    cadv

--- part2-c1unit5/src/hooks/useList.tsx
+++ part2-c1unit5/src/hooks/useList.tsx
@@ -2,6 +2,7 @@ import React from "react"
 export const useList = (arr:any) => {
 
     const[uselist1,uselist2]=React.useState(arr)
+     console.log(uselist1,uselist2)
 
     const push=(x:number)=>{
         uselist1.push(x)
@@ -22,7 +23,8 @@ export const useList = (arr:any) => {
 
     const map=(x:Function)=>{
         console.log(x)
-       let mydata=uselist1.map(x)
+    
+        let mydata=uselist1.map(x)
        uselist2(mydata)
     }
 
